a679b444945ff202305a79db9677ad499e33550e
Jason Sharpton <mister.jae1@gmail.com>
2020-01-11 17:50:09 +0000

    Automatically backed up by Learn

--- lib/a_syntax_error.rb
+++ lib/a_syntax_error.rb
@@ -1,3 +1,8 @@
-x = 1
-
-x = 2
+x = 1 # integer
+name ="Jason" # string 
+likes_ruby = true # boolean
+favorite_number=[1,7,21] # array 
+names = ["Jacky", "Jason", "John"] # array 
+# if i wanted to get the 7 out of these numbers then favorite_number[1]
+puts favorite_number[1]
+puts names[2]
